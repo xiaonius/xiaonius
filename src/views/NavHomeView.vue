4466943eee73dc367ede58db0bb7a56f55784008
xiaoniuss <74769487+xiaonius@users.noreply.github.com>
2025-08-11 19:28:50 +0800

    Update NavHomeView.vue

--- src/views/NavHomeView.vue
+++ src/views/NavHomeView.vue
@@ -77,13 +77,10 @@
           <div class="search-engine-selector">
             <img :src="searchEngines[selectedEngine].icon" :alt="selectedEngine" class="engine-logo" />
             <select v-model="selectedEngine" class="engine-select">
+              <option value="google">Google</option>
               <option value="baidu">百度</option>
               <option value="bing">Bing</option>
-              <option value="google">Google</option>
-              <option value="taobao">淘宝</option>
-              <option value="jingdong">京东</option>
-              <option value="juzipan">橘子盘搜</option>
-              <option value="limao">猫狸盘搜</option>
+              <option value="duckduckgo">DuckDuckGo</option>
             </select>
           </div>
           <input
@@ -213,22 +210,19 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import { useNavigation } from '@/apis/useNavigation.js'
 // 导入搜索引擎logo图片
-import baiduLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import bingLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import googleLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import taobaoLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import jingdongLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import juzipanLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
-import limaoLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
+import googleLogo from '@/assets/goolge.png'
+import baiduLogo from '@/assets/baidu.png'
+import bingLogo from '@/assets/bing.png'
+import duckLogo from '@/assets/duck.png'
 // 导入GitHub logo
-import githubLogo from 'https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png'
+import githubLogo from '@/assets/github.png'
 
 // 使用导航API
 const { categories, title, loading, error, fetchCategories } = useNavigation()
 
 // 响应式数据
 const searchQuery = ref('') // 搜索查询
-const selectedEngine = ref('baidu') // 选中的搜索引擎
+const selectedEngine = ref('bing') // 选中的搜索引擎
 const showMobileMenu = ref(false) // 移动端菜单显示状态
 
 // 锁定功能相关
@@ -240,40 +234,25 @@ const unlockError = ref('') // 解锁错误信息
 
 // 搜索引擎配置
 const searchEngines = {
+  google: {
+    url: 'https://www.google.com/search?q=',
+    icon: googleLogo,
+    placeholder: 'Google 搜索'
+  },
   baidu: {
     url: 'https://www.baidu.com/s?wd=',
     icon: baiduLogo,
-    placeholder: '百度一下(点logo切换搜索引擎)'
+    placeholder: '百度一下'
   },
   bing: {
     url: 'https://www.bing.com/search?q=',
     icon: bingLogo,
     placeholder: 'Bing (点logo切换搜索引擎)'
   },
-  google: {
-    url: 'https://www.google.com/search?q=',
-    icon: googleLogo,
-    placeholder: 'Google 搜索(点logo切换搜索引擎)'
-  },
-  taobao: {
-    url: 'https://s.taobao.com/search?q=',
-    icon: taobaoLogo,
-    placeholder: '输入商品的名字关键词'
-  },
-  jingdong: {
-    url: 'https://search.jd.com/Search?keyword=',
-    icon: jingdongLogo,
-    placeholder: '输入商品的名字关键词'
-  },
-  limao: {
-    url: 'https://www.alipansou.com/search?k=',
-    icon: limaoLogo,
-    placeholder: '输入电视电影资源名字'
-  },
-  juzipan: {
-    url: 'https://www.alipansou.com/search?k=',
-    icon: juzipanLogo,
-    placeholder: '输入电视电影资源名字'
+  duckduckgo: {
+    url: 'https://duckduckgo.com/?q=',
+    icon: duckLogo,
+    placeholder: 'DuckDuckGo 搜索'
   }
 }
 
